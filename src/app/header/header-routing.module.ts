997ec36e812569130121ce4e9ce9e501470c4565
Rajas <rajaaltus@gmail.com>
2019-07-01 08:04:17 +0700

    Build error fixed

--- src/app/header/header-routing.module.ts
+++ src/app/header/header-routing.module.ts
@@ -3,10 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'about', loadChildren: './about/about.module#AboutModule' },
-  { path: 'career', loadChildren: './career/career.module#CareerModule' },
-  { path: 'contact', loadChildren: './contact/contact.module#ContactModule' }
+
 ];
 
 @NgModule({
