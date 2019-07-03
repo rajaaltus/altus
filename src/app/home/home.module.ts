9019c0b1f1e5ca47087c1b75efd3e2147d5db9df
Rajas <rajaaltus@gmail.com>
2019-07-03 19:25:16 +0700

    Build made after pages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
-  declarations: [AboutComponent],
+  declarations: [],
   imports: [
     CommonModule,
     HomeRoutingModule
